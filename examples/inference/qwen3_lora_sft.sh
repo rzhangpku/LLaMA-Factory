466777c82b5961a304124859e4df65a60e4ed1b7
zhangrong01 <zhangrong01@yuaiweiwu.com>
2025-08-18 07:41:38 +0000

    inference

---
+++ examples/inference/qwen3_lora_sft.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python scripts/vllm_infer.py
+    --dataset alpaca_test_data
+    --model_name_or_path /aiplatform-sale/aiplatform-sale/group-shared/models/Qwen/Qwen3-1.7B
+    --adapter_name_or_path saves/Qwen3-1.7B/lora/sft
+    --template qwen3_nothink
+    --finetuning_type lora
+    --infer_backend vllm  # choices: [huggingface, vllm, sglang]
+    --trust_remote_code true
