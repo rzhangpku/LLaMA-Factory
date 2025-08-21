4ff998ddd197fcc71592f013f8a3574da2fcc6c2
zhangrong01 <zhangrong01@yuaiweiwu.com>
2025-08-21 02:13:42 +0000

    api

--- examples/inference/api_call_qwen3_lora_sft.py
+++ examples/inference/api_call_qwen3_lora_sft.py
@@ -2,13 +2,28 @@ from openai import OpenAI
 import time
 import numpy as np
 
+
+def get_prompt_gen(fn) -> str:
+    if not fn:
+        return ""
+    with open(fn, "r") as fr:
+        prompt_gen = fr.read()
+    return prompt_gen
+
+
+system_prompt = get_prompt_gen(
+    "/root/ai-sale-tutor/scripts/spoken_rewrite/data/instruction_prompt.md")
+
+
 def chat(user_sent):
     start_timer = time.time() * 1000
     client = OpenAI(api_key="0", base_url="http://0.0.0.0:8000/v1")
-    messages = [{"role": "user", "content": user_sent}]
+    messages = [{"role": "system", "content": system_prompt}] + \
+        [{"role": "user", "content": user_sent}]
     result = client.chat.completions.create(
         messages=messages, model="/aiplatform-sale/aiplatform-sale/group-shared/models/Qwen/Qwen3-1.7B")
-    print(result.choices[0].message)
+    content = result.choices[0].message.content
+    # print(content)
     end_timer = time.time() * 1000
     elapse_time = end_timer - start_timer
     return elapse_time
@@ -22,6 +37,7 @@ def cal_avg_time():
             elapse_time = chat(user_sent.strip())
             elapse_time_list.append(elapse_time)
     print(f"avg_time: {np.mean(elapse_time_list)}")
+    print(f"median_time: {np.median(elapse_time_list)}")
 
 
 if __name__ == "__main__":
